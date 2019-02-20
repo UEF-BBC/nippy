08c4b65fe11c1340564b2bb98a6a8ee71cd61fc2
jtorniainen <jtorniainen@users.noreply.github.com>
2019-02-20 10:06:20 +0200

    __init__ now imports preprocessor and run_pipeline

--- nippy/__init__.py
+++ nippy/__init__.py
@@ -1,3 +1,3 @@
-from .nippy import nippy, export_pipelines_to_csv, export_pipelines_to_mat, export_pipelines_to_pickle
+from .nippy import nippy, export_pipelines_to_csv, export_pipelines_to_mat, export_pipelines_to_pickle, Preprocessor, run_pipeline
 from .handler import read_configuration
 __all__ = ['nippy', 'handler']
