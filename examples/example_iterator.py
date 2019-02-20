5268c45e83ff0c0e1e7ae21c92d7009b5e9142b6
jtorniainen <jtorniainen@users.noreply.github.com>
2019-02-20 12:08:27 +0200

    added a second example for iterator

---
+++ examples/example_iterator.py
@@ -0,0 +1,23 @@
+# A simple example of how to use the iterator functionality of nippy.
+#
+# jtorniainen // UEF
+# 2019, MIT License
+
+import nippy
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    # 1. Load data
+    data = np.genfromtxt('nir_data.csv', delimiter=',')
+    wavelength = data[0, :]
+    spectra = data[1:, :].T  # Rows = wavelength, Columns = samples
+
+    # 2. Initialize iterator
+    iterator = nippy.Preprocessor(wavelength, spectra, 'example.ini')
+
+    for wavelength_, spectra_, pipe_ in iterator:
+        print(pipe_)
+
+
