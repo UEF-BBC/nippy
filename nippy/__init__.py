5758f13b84688edfdbedeb3b2a3aa072dbdc39a0
jtorniainen <jtorniainen@users.noreply.github.com>
2020-08-13 13:07:58 +0300

    another solution for the namespace issue

--- nippy/__init__.py
+++ nippy/__init__.py
@@ -1,3 +1,4 @@
-from .nippy import nippy, export_pipelines_to_csv, export_pipelines_to_mat, export_pipelines_to_pickle, Preprocessor, run_pipeline
+# from .nippy import nippy, export_pipelines_to_csv, export_pipelines_to_mat, export_pipelines_to_pickle, Preprocessor, run_pipeline
+from .nippy import *
 from .handler import read_configuration
 __all__ = ['nippy', 'handler']
